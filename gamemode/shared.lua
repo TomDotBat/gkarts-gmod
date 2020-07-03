ba63e022c0eea7626b4e6ab34ffdbd2ea4366642
nosharp <nosharphosting@gmail.com>
2020-07-03 15:08:31 +0100

    Module loading ☄

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,9 +1,42 @@
-
 GM.Name = "gKarts"
 GM.Author = "MagmaPlex Team - Tom.bat & NoSharp"
 GM.Email = "N/A"
 GM.Website = "http://magmanet.co/"
 
+-- GM:LoadDirectory("gkarts/gamemode/modules/*")
 function GM:Initialize()
-
+    print("[G-Karts] Loading...")
+    print("[G-Karts] GKarts is property of Magma Networks(Tom.Bat and NoSharp)")
+    GKarts = {}
+    self:LoadDirectory("gkarts/gamemode/modules")
 end
+
+function GM:LoadDirectory(dir)
+    local fil, fol = file.Find(dir .. "/*", "LUA")
+
+    for k, v in pairs(fol) do
+        self:LoadDirectory(dir .. "/" .. v)
+        print("[G-Karts] Loaded folder: \"" .. dir .. "/" .. v .. "\"")
+    end
+
+    for k, v in ipairs(fil) do
+        local dirs = dir .. "/" .. v
+
+        if v:StartWith("cl_") then
+            if SERVER then
+                AddCSLuaFile(dirs)
+            else
+                include(dirs)
+            end
+        elseif v:StartWith("sh_") then
+            AddCSLuaFile(dirs)
+            include(dirs)
+        else
+            if SERVER then
+                include(dirs)
+            end
+        end
+    end
+end
+
+GM:LoadDirectory("gkarts/gamemode/modules")
