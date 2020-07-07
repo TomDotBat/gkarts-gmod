a50b165f7fbe4e172c31b43c316b7e161998fada
nosharp <nosharp1@gmail.com>
2020-07-07 18:41:51 +0100

    Go karts are going brr now

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -6,9 +6,35 @@ include("shared.lua")
 
 
 function GM:PlayerSpawn(ply)
-    local x = ents.Create("prop_physics")
-    x:SetPos(ply:GetPos() + Vector(10,10,10))
-    x:SetModel("models/props_borealis/bluebarrel001.mdl")
-    x:Spawn()
-    drive.PlayerStartDriving( ply, x, "gokart_drive" )
+    
 end
+
+local data =  {
+	Name = "Go-Kart",
+	Model = "models/sligwolf/gokart/sw_gokart.mdl",
+	Class = "prop_gokart",
+	Category = "gokart",
+    Author = "z",
+    Information = "Go-Kart made by SligWolf",
+    Model = "models/sligwolf/gokart/sw_gokart.mdl",
+    SW_Values_GoKart = {},
+    SW_GoKart_Cams = {
+        C1 = {},
+        C2 = {},
+        C3 = {},
+        C4 = {}
+    },
+    SW_GoKart_Helper = {
+        H1 = {}
+    },
+    SW_GoKart_Smoke = {
+        S1 = {}
+    },
+    KeyValues = {
+        vehiclescript = "scripts/vehicles/sligwolf/sw_gokart_new.txt"
+    }
+}
+list.Set( "Vehicles", "gokart", data)
+
+
+concommand.Add("Spawn")
