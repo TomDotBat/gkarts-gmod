62594016a9224a43c59047775445f069f3cd3850
Tom <tom@tomdotbat.dev>
2020-07-07 18:39:04 +0100

    Position colours

--- gamemode/modules/ui/cl_colours.lua
+++ gamemode/modules/ui/cl_colours.lua
@@ -2,5 +2,13 @@
 GM.Colors = {
     PrimaryBackground = Color(33, 32, 32),
     PrimaryForeground = Color(234, 127, 33),
-    PrimaryText = Color(255, 255, 255)
+    PrimaryText = Color(255, 255, 255),
+
+
+    DefaultPosition = Color(255, 255, 255),
+    Positions = {
+        [1] = Color(232, 198, 13),
+        [2] = Color(193, 193, 193),
+        [3] = Color(181, 112, 43)
+    }
 }
