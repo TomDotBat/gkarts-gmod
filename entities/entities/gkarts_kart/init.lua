2d4158856ff3aeff5b273b8c3e777515ab6c80ec
Tom <tom@tomdotbat.dev>
2020-09-07 13:13:39 +0100

    Seat placement and sitting in the vehicle

--- entities/entities/gkarts_kart/init.lua
+++ entities/entities/gkarts_kart/init.lua
@@ -8,9 +8,54 @@ function ENT:Initialize()
     self:SetMoveType(MOVETYPE_VPHYSICS)
     self:SetSolid(SOLID_VPHYSICS)
     self:PhysicsInit(SOLID_VPHYSICS)
+    self:SetUseType(SIMPLE_USE)
 
     local phys = self:GetPhysicsObject()
     if IsValid(phys) then
         phys:Wake()
     end
+
+    hook.Add("PlayerLeaveVehicle", self, self.DestroySeat)
+
+    self:BuildSeat()
+end
+
+function ENT:Use(ply)
+    if not ply:IsPlayer() then return end
+    self:BuildSeat(ply)
+end
+
+function ENT:BuildSeat(driver)
+    if not IsValid(self.Chair) then
+        self.Chair = ents.Create("prop_vehicle_prisoner_pod")
+        self.Chair:SetParent(self)
+
+        self.Chair:SetModel("models/nova/airboat_seat.mdl")
+        self.Chair:SetMoveType(MOVETYPE_NONE)
+        self.Chair:SetKeyValue("vehiclescript", "scripts/vehicles/prisoner_pod.txt")
+        self.Chair:SetKeyValue("limitview","0")
+
+        self.Chair:Spawn()
+        self.Chair:Activate()
+    end
+
+    local chairBone = self:LookupBone("gokart_characterplacement")
+    if not chairBone then return end
+
+    local chairPos, chairAngs = self:GetBonePosition(chairBone)
+    if chairPos == self:GetPos() then
+        chairPos = self:GetBoneMatrix(chairBone):GetTranslation()
+    end
+
+    chairAngs:RotateAroundAxis(chairAngs:Up(), -90) --The bone angles are off on this axis but it works perfectly on vehicle scripts, no idea why
+
+    self.Chair:SetPos(self:WorldToLocal(chairPos) + Vector(0, 14, 0)) --The bone position also has the same problem
+    self.Chair:SetAngles(chairAngs)
+
+    if not IsValid(driver) then return end
+    driver:EnterVehicle(self.Chair)
+end
+
+function ENT:DestroySeat()
+    if IsValid(self.Chair) then self.Chair:Remove() end
 end
