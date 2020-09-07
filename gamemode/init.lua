2336aff9fe181842fbd591d730675ebc4c2b0dab
Tom <tom@tomdotbat.dev>
2020-09-07 12:25:24 +0100

    Delete some test driving code

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -9,32 +9,6 @@ function GM:PlayerSpawn(ply)
 
 end
 
-local data =  {
-    Name = "Go-Kart",
-    Class = "prop_gokart",
-    Category = "gokart",
-    Author = "z",
-    Information = "Go-Kart made by SligWolf",
-    Model = "models/sligwolf/gokart/sw_gokart.mdl",
-    SW_Values_GoKart = {},
-    SW_GoKart_Cams = {
-        C1 = {},
-        C2 = {},
-        C3 = {},
-        C4 = {}
-    },
-    SW_GoKart_Helper = {
-        H1 = {}
-    },
-    SW_GoKart_Smoke = {
-        S1 = {}
-    },
-    KeyValues = {
-        vehiclescript = "scripts/vehicles/sligwolf/sw_gokart_new.txt"
-    }
-}
-list.Set( "Vehicles", "gokart", data)
-
 concommand.Add("asd", function(ply)
     local ent = ents.Create("gkarts_powerup")
     print(ent)
