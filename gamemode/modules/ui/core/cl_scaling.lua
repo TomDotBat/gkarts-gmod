dc04d72c112aa26b706cf2ae545d60ece813e2c8
Tom O'Sullivan <tom@tomdotbat.dev>
2021-03-22 17:51:39 +0000

    Add new scaling functions

---
+++ gamemode/modules/ui/core/cl_scaling.lua
@@ -0,0 +1,27 @@
+
+do
+    local scrh = ScrH
+    local max = math.max
+    function RPGM.Scale(value)
+        return max(value * (scrh() / 1080), 1)
+    end
+end
+
+do
+    local constants = {}
+    local scaledConstants = {}
+    function RPGM.RegisterScaledConstant(varName, size)
+        constants[varName] = size
+        scaledConstants[varName] = RPGM.Scale(size)
+    end
+
+    function RPGM.GetScaledConstant(varName)
+        return scaledConstants[varName]
+    end
+
+    hook.Add("OnScreenSizeChanged", "RPGM.StoreScaledConstants", function()
+        for varName, size in pairs(constants) do
+            scaledConstants[varName] = RPGM.Scale(size)
+        end
+    end)
+end
