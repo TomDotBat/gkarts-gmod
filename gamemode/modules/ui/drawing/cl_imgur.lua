9f0ff7efff5962d67207ccc16d036e8342e4c529
Tom O'Sullivan <tom@tomdotbat.dev>
2021-03-22 18:13:11 +0000

    Add an imgur drawing function

---
+++ gamemode/modules/ui/drawing/cl_imgur.lua
@@ -0,0 +1,64 @@
+
+local progressMat
+local getImgur = gKarts.GetImgur
+getImgur("hX9kSve", function(mat) progressMat = mat end)
+
+local curTime = CurTime
+local setMaterial = surface.SetMaterial
+local setDrawColor = surface.SetDrawColor
+local drawTexturedRectRotated = surface.DrawTexturedRectRotated
+local drawTexturedRect = surface.DrawTexturedRect
+
+local min = math.min
+local function drawProgressWheel(x, y, w, h, col)
+    local progSize = min(w, h)
+    setMaterial(progressMat)
+    setDrawColor(col.r, col.g, col.b, col.a)
+    drawTexturedRectRotated(x + w / 2, y + h / 2, progSize, progSize, -curTime() * 100)
+end
+gKarts.DrawProgressWheel = drawProgressWheel
+
+do
+    local materials = {}
+    local grabbingMaterials = {}
+
+    function gKarts.DrawImgur(x, y, w, h, imgurId, col)
+        if not materials[imgurId] then
+            drawProgressWheel(x, y, w, h, col)
+
+            if grabbingMaterials[imgurId] then return end
+            grabbingMaterials[imgurId] = true
+
+            getImgur(imgurId, function(mat)
+                materials[imgurId] = mat
+                grabbingMaterials[imgurId] = nil
+            end)
+
+            return
+        end
+
+        setMaterial(materials[imgurId])
+        setDrawColor(col.r, col.g, col.b, col.a)
+        drawTexturedRect(x, y, w, h)
+    end
+
+    function gKarts.DrawImgurRotated(x, y, w, h, rot, imgurId, col)
+        if not materials[imgurId] then
+            drawProgressWheel(x - w / 2, y - h / 2, w, h, col)
+
+            if grabbingMaterials[imgurId] then return end
+            grabbingMaterials[imgurId] = true
+
+            getImgur(imgurId, function(mat)
+                materials[imgurId] = mat
+                grabbingMaterials[imgurId] = nil
+            end)
+
+            return
+        end
+
+        setMaterial(materials[imgurId])
+        setDrawColor(col.r, col.g, col.b, col.a)
+        drawTexturedRectRotated(x, y, w, h, rot)
+    end
+end
