9b535b99113eb9340be866daa4ac7255e7c0f75e
Tom O'Sullivan <tom@tomdotbat.dev>
2021-03-22 19:30:56 +0000

    Use better colour names

--- gamemode/modules/ui/cl_colours.lua
+++ gamemode/modules/ui/cl_colours.lua
@@ -1,9 +1,13 @@
 
-GM.Colors = {
-    PrimaryBackground = Color(33, 32, 32),
-    PrimaryForeground = Color(234, 127, 33),
-    PrimaryText = Color(255, 255, 255),
+gKarts.Colors = {
+    Background = Color(33, 32, 32),
+    Primary = Color(234, 127, 33),
 
+    PrimaryText = Color(255, 255, 255),
+    SecondaryText = Color(220, 220, 220),
+
+    Positive = Color(66, 134, 50),
+    Negative = Color(164, 50, 50),
 
     DefaultPosition = Color(255, 255, 255),
     Positions = {
