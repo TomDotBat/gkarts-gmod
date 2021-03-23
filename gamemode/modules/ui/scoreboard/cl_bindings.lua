89fa285af4e04b8562969ac119546173a9970a56
Tom O'Sullivan <tom@tomdotbat.dev>
2021-03-23 16:21:19 +0000

    Fix scoreboard toggling so it doesnt break randomly

--- gamemode/modules/ui/scoreboard/cl_bindings.lua
+++ gamemode/modules/ui/scoreboard/cl_bindings.lua
@@ -1,35 +1,46 @@
 
-local toggled = false
+local toggled = true
 function GM:ScoreboardShow()
     toggled = not toggled
 
-    if toggled then
-        if IsValid(self.Scoreboard) then
-            if self.Scoreboard.Fading then return end
-
-            self.Scoreboard:SetVisible(true)
-            self.Scoreboard.CreateTime = CurTime()
-            self.Scoreboard.Fading = true
-
-            self.Scoreboard:AlphaTo(255, .3, 0, function(anim, pnl)
-                self.Scoreboard.Fading = false
-            end)
-
-            return
-        end
-
+    if not IsValid(self.Scoreboard) then
         self.Scoreboard = vgui.Create("gKarts.Scoreboard")
-    else
-        if IsValid(self.Scoreboard) then
-            if self.Scoreboard.Fading then return end
+    end
 
-            self.Scoreboard.Fading = true
-            self.Scoreboard:AlphaTo(0, .3, 0, function(anim, pnl)
-                self.Scoreboard.Fading = false
-                self.Scoreboard:SetVisible(false)
-            end)
-        end
+    if toggled then
+        self:FadeScoreboard(0, .3, function(scoreboard)
+            scoreboard:SetVisible(false)
+        end)
+    else
+        self.Scoreboard:SetVisible(true)
+        self.Scoreboard:ShowHint()
+        self:FadeScoreboard(255, .3)
     end
 end
+
+function GM:FadeScoreboard(targetAlpha, time, callback)
+    local scoreboard = self.Scoreboard
+    if not IsValid(scoreboard) then return end
+
+    local progress = 0
+    local startAlpha = scoreboard:GetAlpha()
+
+    local frameTime = FrameTime
+    local isValid = IsValid
+    local lerp = Lerp
+
+    hook.Add("PreRender", "gKarts.Scoreboard.FadeAnimation", function()
+        if not isValid(scoreboard) then return end
+        progress = progress + (frameTime() / time)
+        scoreboard:SetAlpha(lerp(progress, startAlpha, targetAlpha))
+
+        if progress >= 1 then
+            if callback then callback(scoreboard) end
+            hook.Remove("PreRender", "gKarts.Scoreboard.FadeAnimation")
+        end
+    end)
+end
+
+GAMEMODE.Scoreboard:Remove()
 
 function GM:ScoreboardHide() end
