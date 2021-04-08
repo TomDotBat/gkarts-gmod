b41912be99e9eabb28ca993b254b08a0f67cd6a2
Tom O'Sullivan <tom@tomdotbat.dev>
2021-04-08 17:48:12 +0100

    feat: add a kart metatable class

---
+++ gamemode/modules/classes/sh_kart.lua
@@ -0,0 +1,43 @@
+
+local _R = debug.getregistry()
+
+local kart = _R["gKart"] or {}
+kart.__index = kart
+
+AccessorFunc(kart, "pDriver", "Driver")
+
+_R["gKart"] = kart
+
+
+gKarts.Karts = gKarts.Karts or setmetatable({}, {
+    __mode = "k"
+})
+
+function _R.Player:GetKart()
+    return gKarts.Karts[self]
+end
+
+function gKarts.GetKart(ply)
+    return gKarts.Karts[ply]
+end
+
+function gKarts.CreateKart(ply)
+    if IsValid(gKarts.Karts[ply]) then return gKarts.Karts[ply] end
+
+    local ent = ents.CreateClientProp("models/freeman/vehicles/electric_go-kart.mdl")
+
+    for key, value in pairs(kart) do
+        ent[key] = value
+    end
+
+    ent:SetDriver(ply)
+    ent:Spawn()
+
+    gKarts.Karts[ply] = ent
+
+    if CLIENT and ply == LocalPlayer() then
+        gKarts.LocalKart = ent
+    end
+
+    return ent
+end
