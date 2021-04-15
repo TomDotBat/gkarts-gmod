7d51ff273828afa43199a3057e5b4f3f8716dae1
Tom O'Sullivan <tom@tomdotbat.dev>
2021-04-15 20:58:39 +0100

    refactor: add animation think as a think method

--- gamemode/modules/kart/cl_animations.lua
+++ gamemode/modules/kart/cl_animations.lua
@@ -5,3 +5,5 @@ local STEER_POSE = "vehicle_steer"
 function kart:AnimationThink()
     self:SetPoseParameter(STEER_POSE, self.nSteering)
 end
+
+gKarts.AddKartThinkMethod("animation", kart.AnimationThink)
