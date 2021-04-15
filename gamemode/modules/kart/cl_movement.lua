5da0e32649402f6c3e2f5e8822360ab5af6a2bc7
Tom O'Sullivan <tom@tomdotbat.dev>
2021-04-15 20:22:23 +0100

    feat: movement input handling

---
+++ gamemode/modules/kart/cl_movement.lua
@@ -0,0 +1,75 @@
+
+hook.Add("gKarts.KartCreated", "gKarts.MovementInput", function(kart, driver, isLocal)
+    if not isLocal then return end
+
+    hook.Add("Move", kart, function(_,  _, mv )
+        kart:SetTargetThrottle(mv:GetForwardSpeed() / 10000)
+        kart:SetTargetSteering(mv:GetSideSpeed() / 10000)
+
+        kart:AnimationThink()
+    end)
+end)
+
+do
+    local kart = debug.getregistry()["gKart"]
+    local lerp = Lerp
+    local frameTime = FrameTime
+
+    AccessorFunc(kart, "nSpeed", "Speed", FORCE_NUMBER)
+    AccessorFunc(kart, "nThrottle", "Throttle", FORCE_NUMBER)
+    AccessorFunc(kart, "nSteering", "Steering", FORCE_NUMBER)
+
+    AccessorFunc(kart, "nTargetThrottle", "TargetThrottle", FORCE_NUMBER)
+    AccessorFunc(kart, "nTargetSteering", "TargetSteering", FORCE_NUMBER)
+
+    do
+        local traceLine = util.TraceLine
+
+        local traceData = {}
+        local HOVER_DISTANCE = Vector(0, 0, 4)
+
+        function kart:GetHoverTrace()
+            local pos = self:GetPos()
+
+            traceData["start"] = pos
+            traceData["endpos"] = pos - HOVER_DISTANCE
+
+            return traceLine(traceData)
+        end
+
+        function kart:GetGroundDistance()
+            return self:GetPos() - self:GetHoverTrace().HitPos
+        end
+    end
+
+    function kart:ApplyHoverForce()
+        self:GetPhysicsObject():ApplyForceCenter(Vector(0, 0, self:GetHoverTrace().Fraction  * FrameTime()))
+    end
+
+    function kart:MovementThink()
+        local prog = frameTime() * 4
+        self.nThrottle = lerp(prog, self.nThrottle, self.nTargetThrottle)
+        self.nSteering = lerp(prog, self.nSteering, self.nTargetSteering)
+
+        local physObj = self:GetPhysicsObject()
+
+        local velocity = physObj:GetVelocity()
+        velocity[3] = 0
+
+        physObj:SetVelocityInstantaneous(velocity)
+
+        --self:ApplyHoverForce()
+    end
+end
+
+hook.Add("gKarts.KartCreated", "gKarts.MovementThink", function(kart)
+    kart:SetSpeed(0)
+
+    kart:SetThrottle(0)
+    kart:SetSteering(0)
+
+    kart:SetTargetThrottle(0)
+    kart:SetTargetSteering(0)
+
+    hook.Add("Think", kart, kart.MovementThink)
+end)
