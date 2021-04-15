7e6203197e08ca7f7de4b4d5db9534aae38feb4f
Tom O'Sullivan <tom@tomdotbat.dev>
2021-04-15 20:21:36 +0100

    feat: (temp) clientside kart builder

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,2 +1,32 @@
 
 include("shared.lua")
+
+local localPly = LocalPlayer()
+if not IsValid(localPly) then return end
+
+if not IsValid(TestKart) then
+    TestKart = gKarts.CreateKart(localPly)
+    TestKart:SetPos(localPly:GetEyeTrace().HitPos + Vector(0, 0, 80))
+    --gKarts.AttachCamera(TestKart)
+
+    --timer.Simple(0, function()
+    --    TestKart:SetDriverModel("models/odessa.mdll")
+    --end)
+else
+    TestKart:SafeRemove()
+end
+
+--local ragdoll = ClientsideRagdoll( "models/gman_high.mdl" )
+--ragdoll:SetPos(localPly:GetPos())
+--ragdoll:SetNoDraw( false )
+--ragdoll:DrawShadow( true )
+--
+--local pos = localPly:GetPos()
+--for i=0, ragdoll:GetPhysicsObjectCount() - 1 do
+--	local phys = ragdoll:GetPhysicsObjectNum(i)
+--	phys:SetPos(pos, true)
+--end
+
+--if IsValid(testThingy) then testThingy:Remove() end
+--testThingy = ents.CreateClientside("phys_spring")
+--print(testThingy)
