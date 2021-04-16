e18a7f0580f2555057b1cb002270b45457facdbd
Tom O'Sullivan <tom@tomdotbat.dev>
2021-04-16 21:49:11 +0100

    feat: kart gravity + wall/ceiling sticking

--- gamemode/modules/kart/cl_physics.lua
+++ gamemode/modules/kart/cl_physics.lua
@@ -1,4 +1,33 @@
 
+do
+    local kart = debug.getregistry()["gKart"]
+    local GRAVITY_VECTOR = physenv.GetGravity()
+
+    do
+        local traceLine = util.TraceLine
+        local GRAVITY_STICK_DISTANCE = gKarts.Constants.KART_GRAVITY_STICK_DISTANCE
+
+        local traceData = {}
+
+        function kart:GetGravityTrace()
+            traceData["start"] = self:GetPos()
+            traceData["endpos"] = self:LocalToWorld(GRAVITY_STICK_DISTANCE)
+            return traceLine(traceData)
+        end
+    end
+
+    function kart:GetGravityVector()
+        return self:GetGravityTrace().Hit and (self:GetUp() * GRAVITY_VECTOR) or GRAVITY_VECTOR
+    end
+
+    function kart:GravityThink()
+        local physObj = self:GetPhysicsObject()
+        physObj:ApplyForceCenter(self:GetGravityVector() * physObj:GetMass() * FrameTime())
+    end
+
+    gKarts.AddKartThinkMethod("gravity", kart.GravityThink)
+end
+
 hook.Add("gKarts.KartCreated", "gKarts.PhysicsSetup", function(kart, _, isLocal)
     local physObj = kart:GetPhysicsObject()
     physObj:EnableGravity(false)
