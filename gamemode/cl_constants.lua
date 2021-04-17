11c72808b707ba3162939b8e7104f389fd412fe8
Tom O'Sullivan <tom@tomdotbat.dev>
2021-04-17 12:23:22 +0100

    feat: support lua refresh on constants

--- gamemode/cl_constants.lua
+++ gamemode/cl_constants.lua
@@ -1,4 +1,7 @@
 
-gKarts.Constants = {
-    KART_GRAVITY_STICK_DISTANCE = Vector(0, 0, -40)
+gKarts.Constants = gKarts.Constants or {
+    KART_GRAVITY_STICK_DISTANCE = Vector(0, 0, -40),
+    KART_HOVER_HEIGHT = Vector(0, 0, 4),
+    KART_STEERING_SENSITIVITY = 4,
+    KART_THROTTLE_SENSITIVITY = 4
 }
