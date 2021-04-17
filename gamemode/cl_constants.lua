997c3dd0f0aa426dae2fae0643254163879860ab
Tom O'Sullivan <tom@tomdotbat.dev>
2021-04-17 15:14:59 +0100

    fix: store hover force as a vector

--- gamemode/cl_constants.lua
+++ gamemode/cl_constants.lua
@@ -2,7 +2,7 @@
 gKarts.Constants = gKarts.Constants or {
     KART_GRAVITY_STICK_DISTANCE = Vector(0, 0, -40),
     KART_HOVER_HEIGHT = 4,
-    KART_HOVER_FORCE = 200,
+    KART_HOVER_FORCE = Vector(0, 0, 200),
     KART_STEERING_SENSITIVITY = 4,
     KART_THROTTLE_SENSITIVITY = 4
 }
