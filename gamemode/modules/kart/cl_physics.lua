45623703be60b84db904223f5e6bb059b8b0de8b
Tom O'Sullivan <tom@tomdotbat.dev>
2021-04-19 18:12:06 +0100

    feat: (temp) kart hovering mechanic

--- gamemode/modules/kart/cl_physics.lua
+++ gamemode/modules/kart/cl_physics.lua
@@ -11,6 +11,7 @@ do
         local traceData = {}
 
         function kart:GetGravityTrace()
+            debugoverlay.Sphere(self:GetPos(), 4, 1, HSVToColor((CurTime() * 50) % 360, 1, 1), true)
             traceData["start"] = self:GetPos()
             traceData["endpos"] = self:LocalToWorld(GRAVITY_STICK_DISTANCE)
             return traceLine(traceData)
@@ -19,14 +20,26 @@ do
 
     function kart:GetGravityVector(gravityTrace)
         return (gravityTrace or self:GetGravityTrace()).Hit
-            and (GRAVITY * self:GetUp())
+            and (GRAVITY * gravityTrace.HitNormal)
             or GRAVITY_VECTOR
     end
 
     function kart:GetGroundDistance(gravityTrace)
         if not gravityTrace then gravityTrace = self:GetGravityTrace() end
         if not gravityTrace.Hit then return end
-        return gravityTrace.HitPos[3] - self:GetPos()[3]
+        return self:GetPos()[3] - gravityTrace.HitPos[3]
+    end
+
+    do
+        local HOVER_HEIGHT = gKarts.Constants.KART_HOVER_HEIGHT
+
+        function kart:GetHoverHeight()
+            return HOVER_HEIGHT
+        end
+    end
+
+    function kart:IsBelowHoverHeight(height, gravityTrace)
+        return height < self:GetHoverHeight()
     end
 
     do
@@ -39,16 +52,40 @@ do
         end
     end
 
-    function kart:GravityThink()
-        local gravityTrace = self:GetGravityTrace()
+    function kart:ApplyGravityForce(gravityTrace)
         local physObj = self:GetPhysicsObject()
         physObj:ApplyForceCenter(self:GetGravityForce(physObj, gravityTrace))
     end
 
-    gKarts.AddKartThinkMethod("gravity", kart.GravityThink)
+    do
+        local lerp = Lerp
+        local HOVER_FORCE = gKarts.Constants.KART_HOVER_FORCE
+
+        function kart:ApplyHoverForce(gravityTrace)
+            local groundDistance = self:GetGroundDistance(gravityTrace)
+            if not groundDistance then return end
+            if not self:IsBelowHoverHeight(groundDistance, gravityTrace) then return end
+
+            print(1 - (groundDistance / self:GetHoverHeight()), lerp(1 - (groundDistance / self:GetHoverHeight()), 0, HOVER_FORCE) * self:GetUp())
+            self:GetPhysicsObject():ApplyForceCenter(lerp(groundDistance / self:GetHoverHeight(), HOVER_FORCE, 0) * self:GetUp())
+            return true
+        end
+    end
+
+    function kart:PhysicsThink()
+        local gravityTrace = self:GetGravityTrace()
+
+        --self:ApplyHoverForce(gravityTrace)
+        self:ApplyGravityForce(gravityTrace)
+    end
+
+    gKarts.AddKartThinkMethod("physics", kart.PhysicsThink)
 end
 
 hook.Add("gKarts.KartCreated", "gKarts.PhysicsSetup", function(kart)
     local physObj = kart:GetPhysicsObject()
     physObj:EnableGravity(false)
+    physObj:SetMass(gKarts.Constants.KART_MASS)
+    --physObj:SetDamping(100, 100)
+    kart:SetFriction(.5)
 end)
